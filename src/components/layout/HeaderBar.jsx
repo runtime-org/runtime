e08f9e69c84acc83ecb703a0b7eeb631838df8a2
Tito hiera <titohiera@gmail.com>
2025-08-11 01:58:14 +0200

    styled the tabPicker

--- src/components/layout/HeaderBar.jsx
+++ src/components/layout/HeaderBar.jsx
@@ -24,7 +24,7 @@ export default function HeaderBar({ title, leftAction, rightAction }) {
             <h1 className="text-lg font-medium text-white truncate">{title}</h1>
             
             {/* model selector */}
-            <div 
+            {/* <div 
                 onClick={rightAction?.onClick}
                 className="flex items-center gap-2 px-3 py-1 rounded-md hover:bg-[#323232] cursor-pointer transition-colors whitespace-nowrap flex-shrink-0"
             >
@@ -39,7 +39,7 @@ export default function HeaderBar({ title, leftAction, rightAction }) {
                 >
                     <path fillRule="evenodd" d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z" clipRule="evenodd" />
                 </svg>
-            </div>
+            </div> */}
         </header>
     )
 }
