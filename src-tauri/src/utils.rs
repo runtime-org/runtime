63389580bc9c5b0afcdc551cef405ab03589ac64
Tito hiera <titohiera@gmail.com>
2025-08-12 00:19:15 +0200

    clean utils.rs

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -16,37 +16,3 @@ pub fn check_browser(id: &str, name: &str, paths: &[&str]) -> Option<BrowserConf
     }
     None
 }
-
-
-// pub async fn download_and_extract(url: String) -> Result<String, String> {
-//     /*
-//     fetch the byte in async mode
-//     */
-//     let response = reqwest::Client::new()
-//         .get(url)
-//         .send()
-//         .await
-//         .map_err(|e| e.to_string())?
-//         .error_for_status()
-//         .map_err(|e| e.to_string())?;
-    
-//     let bytes = response
-//         .bytes()
-//         .await
-//         .map_err(|e| e.to_string())?;
-//     /*
-//     run the CPU-bound file parsing on a blocking thread
-//     */
-//     let text = tauri::async_runtime::spawn_blocking(move || {
-//         let extractor = Extractor::new()
-//             .set_extract_string_max_length(20_000_000); // 20MB
-
-//         extractor
-//             .extract_bytes_to_string(&bytes)
-//             .map(|(t, _meta)| t)
-//             .map_err(|e| e.to_string())
-//     }).await
-//     .map_err(|e| e.to_string())??;
-
-//     Ok(text)
-// }
